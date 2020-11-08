ebf0fefe65215d1dfbdad42b3c41d43a571ba4a2
Fahim Ahmed <fahim52510@gmail.com>
2020-11-08 22:26:23 +0600

    top-menu updated

--- src/components/molecule/navigation/Header.vue
+++ src/components/molecule/navigation/Header.vue
@@ -1,35 +1,33 @@
 <template>
 	<div class="bg-white">
 		<div class="container mx-auto py-3">
-			<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3">
-				<div class="flex justify-between">
-					<div class="md:flex-shrink-0">
-						<img src="/images/logo.png" alt="">
-					</div>
+			<div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:grid-cols-6 gap-3">
+				<div class="col-span-2">
+					<img class="inline-block" src="/images/logo.png" alt="">
 					<button 
-						class="bg-gray-800 rounded text-lg text-white uppercase hover:bg-blue-600 focus:outline-none px-4 py-2">
+						class="bg-gray-800 w-42 rounded text-lg text-white uppercase hover:bg-blue-600 focus:outline-none px-4 py-2">
 						<span class="mdi mdi-menu pr-3"></span> <span class="text-sm font-semibold">All categories</span>
 					</button>
 				</div>
-				<div class="flex md:col-span-2 lg:col-auto">
-					<div class=" flex w-full border-solid border border-blue-400 rounded p-1">
-						<input placeholder="Search by text" 
-						class="hover:bg-white focus:border-blue-400 focus:outline-none w-full px-3" type="text">
-						<span class="mdi text-3xl mdi-magnify bg-blue-400 rounded " style="color: #fff; padding: 4px"></span>
+				<div class="col-span-3">
+					<div class="flex items-start w-full border-solid border border-blue-400 rounded relative" style="padding: 8px">
+						<input placeholder="Search product by title, brand, materials, supplies, category, etc..." 
+						class="hover:bg-white focus:border-blue-400 focus:outline-none w-full" type="text">
+						<span class="mdi text-xl mdi-magnify bg-blue-400 rounded absolute cursor-pointer" style="color: #fff; padding: 1px 4px; top: 3px; right: 3px"></span>
 					</div>
 				</div>
-				<div class="sm:col-span-3 lg:col-auto">
+				<div class="col-span-1">
 					<div class="flex justify-between">
 						<router-link class="flex items-center text-gray-700" to="#">
 							<img width="30" src="/images/icons/circled_menu.svg" alt="">
 						</router-link> 
-						<router-link class="flex items-center text-gray-700" to="#">
+						<router-link class="flex w-10 items-center text-gray-700" to="#">
 							<span class="mdi text-3xl mdi-cart"></span>
-							<span>Place Req</span>
+							<span class="text-xs">Place Req</span>
 						</router-link> 
-						<router-link class="flex items-center text-gray-700" to="#">
+						<router-link class="flex w-10 items-center text-gray-700" to="#">
 							<span class="mdi text-3xl mdi-view-list"></span>
-							<span>Rep List</span>
+							<span class="text-xs">Rep List</span>
 						</router-link> 
 						<router-link class="flex items-center text-gray-700" to="#">
 							<span class="mdi text-3xl mdi-account-circle"></span>
