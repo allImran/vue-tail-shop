841f399112a7e9b2a20867e954f4956a1a315acf
allImran <badhan19@gmail.com>
2020-11-19 23:01:29 +0600

    certificate, sidebar, about on profile

---
+++ src/components/molecule/profile/DetailAndSidebar.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="container mx-auto mt-5 mb-5">
+        <div class="grid grid-cols-3 gap-10 rounded">
+            <div class="col-span-2 bg-white shadow-md pt-4">
+                <TopBar 
+                    tabMargin="ml-5 uppercase font-semibold"
+                />
+                <About />
+                <Certificate />
+            </div>
+            <div class="">
+                <Sidebar />
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+    import TopBar from '@/components/molecule/navigation/TopBar'
+    import About from '@/components/molecule/profile/About'
+    import Certificate from '@/components/molecule/profile/Certificate'
+    import Sidebar from '@/components/molecule/profile/Sidebar'
+  export default {
+    name: '',
+    components:{
+        TopBar,
+        About,
+        Sidebar,
+        Certificate
+    },
+    props:{
+
+    },
+    data(){
+      return{
+
+      }
+    },
+    computed:{
+
+    },
+    watch:{
+
+    },
+    methods:{
+
+    }
+  }
+</script>
+
+<style>
+  
+</style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,14 +1,16 @@
 <template>
-	<PhotoAndDesSection />
+	<PhotoAndDescriptionSection />
+	<DetailAndSidebarSection />
 </template>
 
 <script>
-	import PhotoAndDesSection from '@/components/molecule/profile/PhotoAndDes'
-	// import PhotoAndDesSection from '@/components/molecule/profile/PhotoAndDes'
+	import PhotoAndDescriptionSection from '@/components/molecule/profile/PhotoAndDes'
+	import DetailAndSidebarSection from '@/components/molecule/profile/DetailAndSidebar'
 	export default{
 		name: 'Vendor',
 		components: {
-			PhotoAndDesSection
+			PhotoAndDescriptionSection,
+			DetailAndSidebarSection	
 		}
 	}
 </script>
