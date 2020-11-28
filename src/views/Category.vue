6ab1a84f6e181e3a2556573bd243cd6162ba654c
Lemon Patwari <lemonpatwari@gmail.com>
2020-11-28 22:38:40 +0600

    slider add in category page

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -1,19 +1,23 @@
 <template>
-	<BrandSection />
-	<CategorySection />
-	<ProductByCategorySection />
+  <SliderSection/>
+  <BrandSection/>
+  <CategorySection/>
+  <ProductByCategorySection/>
 </template>
 
 <script>
-	import BrandSection from "@/components/molecule/BrandList";
-	import CategorySection from "@/components/molecule/category/CategoriesInCard.vue"
-	import ProductByCategorySection from "@/components/molecule/product/ProductByCategory.vue"
-	export default {
-		name: 'CategoryPage',
-		components: {
-			BrandSection,
-			CategorySection,
-			ProductByCategorySection
-		}
-	}
+import BrandSection from "@/components/molecule/BrandList";
+import CategorySection from "@/components/molecule/category/CategoriesInCard.vue"
+import ProductByCategorySection from "@/components/molecule/product/ProductByCategory.vue"
+import SliderSection from "@/components/molecule/category/SliderSection";
+
+export default {
+  name: 'CategoryPage',
+  components: {
+    BrandSection,
+    CategorySection,
+    ProductByCategorySection,
+    SliderSection,
+  }
+}
 </script>
