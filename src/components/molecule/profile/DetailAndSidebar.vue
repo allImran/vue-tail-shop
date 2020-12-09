4ebb61914ab059fc1bc62b14caf55735a83c5d41
Lemon Patwari <lemonpatwari@gmail.com>
2020-12-09 19:58:30 +0600

    slider modified

--- src/components/molecule/profile/DetailAndSidebar.vue
+++ src/components/molecule/profile/DetailAndSidebar.vue
@@ -1,68 +1,59 @@
 <template>
-    <div class="container mx-auto mt-5 mb-5">
-        <div class="grid grid-cols-3 gap-10 rounded">
-            <div class="col-span-3 sm:col-span-2 bg-white shadow-md pt-4">
-                <TopBar 
-                    tabMargin="ml-5 uppercase font-semibold"
-                />
+  <div class="container mx-auto mt-5 mb-5">
+    <div class="grid grid-cols-3 gap-10 rounded">
+      <div class="col-span-3 sm:col-span-2 bg-white shadow-md pt-4">
+        <TopBar
+            tabMargin="ml-5 uppercase font-semibold"
+        />
 
-                <About />
-                <Skill />
-                <ProjectSlider />
-                <Certificate />
-                <Experience />
-                <Education />
+        <About/>
+        <Skill/>
+        <ProjectSlider/>
+        <Certificate/>
+        <Experience/>
+        <Education/>
 
-            </div>
-            <div class="col-span-3 sm:col-span-1">
-                <Sidebar />
-            </div>
-        </div>
+      </div>
+      <div class="col-span-3 sm:col-span-1">
+        <Sidebar/>
+      </div>
     </div>
-    
+  </div>
+
 </template>
 
 <script>
-    import TopBar from '@/components/molecule/navigation/TopBar'
-    import About from '@/components/molecule/profile/About'
-    import Skill from '@/components/molecule/profile/Skill'
-    import Certificate from '@/components/molecule/profile/Certificate'
-    import Experience from '@/components/molecule/profile/Experience'
-    import Education from '@/components/molecule/profile/Education'
-    import Sidebar from '@/components/molecule/profile/Sidebar'
-    import ProjectSlider from '@/components/molecule/profile/ProjectSlide'
-  export default {
-    name: '',
-    components:{
-        TopBar,
-        About,
-        Skill,
-        Sidebar,
-        Certificate,
-        Experience,
-        Education,
-        ProjectSlider
-    },
-    props:{
+import TopBar from '@/components/molecule/navigation/TopBar'
+import About from '@/components/molecule/profile/About'
+import Skill from '@/components/molecule/profile/Skill'
+import Certificate from '@/components/molecule/profile/Certificate'
+import Experience from '@/components/molecule/profile/Experience'
+import Education from '@/components/molecule/profile/Education'
+import Sidebar from '@/components/molecule/profile/Sidebar'
+import ProjectSlider from '@/components/molecule/profile/ProjectSlide'
 
-    },
-    data(){
-      return{
-
-      }
-    },
-    computed:{
-
-    },
-    watch:{
-
-    },
-    methods:{
-
-    }
-  }
+export default {
+  name: '',
+  components: {
+    TopBar,
+    About,
+    Skill,
+    Sidebar,
+    Certificate,
+    Experience,
+    Education,
+    ProjectSlider
+  },
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  methods: {}
+}
 </script>
 
 <style>
-  
+
 </style>
