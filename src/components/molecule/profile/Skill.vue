b4b1f853a906a5717e9ab84354bb8513ec5b1cc3
Lemon Patwari <lemonpatwari@gmail.com>
2020-12-10 22:21:33 +0600

    profile page carousel add

--- src/components/molecule/profile/Skill.vue
+++ src/components/molecule/profile/Skill.vue
@@ -1,71 +1,64 @@
 <template>
-    <div class="px-5 pb-5 mt-5 ">
-        <TextWithIcon
-            text="Skills"
-            textStyle="capitalize text-lg text-blue-600 font-semibold"
-            icon="mdi mdi-account"
-            iconStyle=" text-xl text-blue-600"
-        />
-        <div class="px-5 mt-5 pb-10 border-b">
-          <div class="flex flex flex-wrap">
-            <div
-                v-for="(item, i) in skills"
-                :key="i"
-                class="mr-5 mb-4"
-            >
-                <div class="flex flex-col items-center">
-                    <p class="text-sm text-gray-700 mb-3">{{ item.title }}</p>
-                    <CircularProgress :percentage="item.percentage"/>
-                </div>
-            </div>
-          </div>
-
-
-          <div class="flex mt-3">
-              <p
-                v-for="(n, i) in 5"
-                :key="i"
-                class="bg-gray-300 mr-2 font-semibold text-xs text-gray-800 px-3 rounded py-1"
-            >
-                Skill Set
-              </p>
+  <div class="px-5 pb-5 mt-5 ">
+    <TextWithIcon
+        text="Skills"
+        textStyle="capitalize text-lg text-blue-600 font-semibold"
+        icon="mdi mdi-account"
+        iconStyle=" text-xl text-blue-600"
+    />
+    <div class="px-5 mt-5 pb-10 border-b">
+      <div class="flex flex flex-wrap">
+        <div
+            v-for="(item, i) in skills"
+            :key="i"
+            class="mr-5 mb-4"
+        >
+          <div class="flex flex-col items-center">
+            <p class="text-sm text-gray-700 mb-3">{{ item.title }}</p>
+            <CircularProgress :percentage="item.percentage"/>
           </div>
         </div>
+      </div>
+
+
+      <div class="flex mt-3">
+        <p
+            v-for="(n, i) in 5"
+            :key="i"
+            class="bg-gray-300 mr-2 font-semibold text-xs text-gray-800 px-3 rounded py-1"
+        >
+          Skill Set
+        </p>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 import TextWithIcon from '@/components/atom/TextWithIcon'
 import CircularProgress from '@/components/atom/CircularProgress'
 import {skills} from '@/data/skill'
-  export default {
-    name: 'Skill',
-    components:{
-        TextWithIcon,
-        CircularProgress
-    },
-    props:{
 
-    },
-    data(){
-      return{
-
-      }
-    },
-    computed:{
-        skills() {
-            return skills;
-        }
-    },
-    watch:{
-
-    },
-    methods:{
-
+export default {
+  name: 'Skill',
+  components: {
+    TextWithIcon,
+    CircularProgress
+  },
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {
+    skills() {
+      return skills;
     }
-  }
+  },
+  watch: {},
+  methods: {}
+}
 </script>
 
 <style>
-  
+
 </style>
---
+++ src/config/experienceCarousel.js
@@ -0,0 +1,15 @@
+export const experienceCarousel = [
+    {
+        width: 0,
+        item: 1
+    }, {
+        width: 768,
+        item: 3
+    }, {
+        width: 1024,
+        item: 3
+    }, {
+        width: 1280,
+        item: 4
+    },
+]
